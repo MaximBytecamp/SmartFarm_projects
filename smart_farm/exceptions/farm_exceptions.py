67fc2472f5be1cba93561018d03b8c79b6824226
MaximBytecamp <bytecampmm@gmail.com>
2026-06-15 19:54:56 +0300

    feat: добавлены исключения для обработки ошибок в модели FarmObject

---
+++ smart_farm/exceptions/farm_exceptions.py
@@ -0,0 +1,20 @@
+class InvalidHealthLevelError(Exception):
+    pass
+
+
+class InvalidTaskPriorityError(Exception):
+    pass
+
+
+class ResourceNotEnoughError(Exception):
+    pass
+
+
+class ObjectAlreadyExistsError(Exception):
+    pass
+
+
+class ObjectNotFoundError(Exception):
+    pass
+
+
